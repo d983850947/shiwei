26e43f07989a91510c9b848c03cd7b9f0adbb9bd
yan.zou02 <yan.zou02@hand-china.com>
2024-03-20 12:08:02 +0800

    Initial commit

---
+++ shiwei/src/main/java/top/shiwei/sw/ShiweiApplication.java
@@ -0,0 +1,13 @@
+package top.shiwei.sw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiweiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiweiApplication.class, args);
+    }
+
+}
---
+++ shiwei/src/test/java/top/shiwei/sw/ShiweiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.shiwei.sw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiweiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
